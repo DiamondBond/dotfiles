dbdfcc1b143c0403e463c1baf94634e31bab3728
Diamond Bond <diamondbond1@gmail.com>
2019-10-19 17:45:16 +0530

    xbindkeys config

---
+++ xbindkeysrc.scm
@@ -0,0 +1,3 @@
+  (xbindkey '("XF86AudioRaiseVolume") "amixer -D pulse sset Master 5%+")
+  (xbindkey '("XF86AudioLowerVolume") "amixer -D pulse sset Master 5%-")
+  (xbindkey '("XF86AudioMute") "amixer set Master toggle")
