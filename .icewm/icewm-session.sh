b6640dd7a89e9c4a27b2d3b6a5c17617a1ccad52
Diamond Bond <diamond.nigel75@Gmail.com>
2022-04-30 20:59:44 +0530

    update

--- .icewm/icewm-session.sh
+++ .icewm/icewm-session.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+# Source DBus
+if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+ for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+  [ -x "$f" ] && . "$f"
+ done
+ unset f
+fi
+
+# Start IceWM with DBus
 exec dbus-launch --sh-syntax --exit-with-session icewm-session
