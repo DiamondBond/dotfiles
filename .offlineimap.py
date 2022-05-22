b5ab032f6d42cca657a5ae092712dc68dc668fbe
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-22 12:06:14 +0530

    Automatic sync
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- .offlineimap.py
+++ .offlineimap.py
@@ -4,5 +4,5 @@ import re, os
 def get_password_emacs(machine, login, port):
     s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
     p = re.compile(s)
-    authinfo = os.popen("gpg -q --no-tty -d ~/.authinfo.gpg").read()
+    authinfo = os.popen("gpg -q --no-tty -d ~/org/.authinfo.gpg").read()
     return p.search(authinfo).group(1)
